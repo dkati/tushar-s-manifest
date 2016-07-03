1343b0d972e890f2d8be990f2ca196e5e7198c66
Tushar Jain <catusharjain95@gmail.com>
2016-07-03 18:16:36 +0530

    Update sultans_cloner.sh

--- sultans_cloner.sh
+++ sultans_cloner.sh
@@ -13,9 +13,9 @@ git clone https://www.github.com/CyanogenMod/android_device_oppo_common -b cm-13
 git clone https://www.github.com/CyanogenMod/android_device_qcom_common -b cm-13.0 device/qcom/common
 git clone https://www.github.com/sultanxda/android_device_oneplus_bacon -b cm-13.0-caf device/oneplus/bacon
 git clone https://www.github.com/sultanxda/android_external_sony_boringssl-compat -b cm-13.0 external/sony/boringssl-compat
-git clone https://www.github.com/sultanxda/android_hardware_qcom_audio -b cm-13.0-caf hardware/qcom/audio/msm8974
-git clone https://www.github.com/sultanxda/android_hardware_qcom_media -b cm-13.0-caf hardware/qcom/media/msm8974
-git clone https://www.github.com/sultanxda/android_hardware_qcom_display -b cm-13.0-caf hardware/qcom/display/msm8974
+git clone https://www.github.com/sultanxda/android_hardware_qcom_audio -b cm-13.0-caf hardware/qcom/audio-caf/msm8974
+git clone https://www.github.com/sultanxda/android_hardware_qcom_media -b cm-13.0-caf hardware/qcom/media-caf/msm8974
+git clone https://www.github.com/sultanxda/android_hardware_qcom_display -b cm-13.0-caf hardware/qcom/display-caf/msm8974
 git clone https://www.github.com/sultanxda/android_hardware_ril -b cm-13.0-legacy hardware/ril-caf
 git clone https://www.github.com/sultanxda/android_kernel_oneplus_msm8974 -b cm-13.0-caf kernel/oneplus/msm8974
 git clone https://www.github.com/sultanxda/proprietary_vendor_oneplus -b cm-13.0-caf vendor/oneplus
