5e09c0070d75405fb4e9c557f7fb710edbb726ca
Tushar Jain <catusharjain95@gmail.com>
2016-11-08 11:01:54 +0530

    Nougat time

--- cloner.sh
+++ cloner.sh
@@ -4,8 +4,8 @@ rm -rf device/oppo/common
 rm -rf device/qcom/common
 rm -rf kernel/oneplus/msm8974
 rm -rf vendor/oneplus
-git clone https://github.com/CyanogenMod/android_kernel_oneplus_msm8974.git -b cm-13.0 kernel/oneplus/msm8974
-git clone https://github.com/CyanogenMod/android_device_oneplus_bacon.git -b cm-13.0 device/oneplus/bacon
-git clone https://github.com/CyanogenMod/android_device_qcom_common.git -b cm-13.0 device/qcom/common
-git clone https://github.com/CyanogenMod/android_device_oppo_common.git -b cm-13.0 device/oppo/common
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus.git -b cm-13.0 vendor/oneplus
+git clone https://gitlab.com/amardeep434/nkernel -b nougat kernel/oneplus/msm8974
+git clone https://gitlab.com/amardeep434/ndevice -b cm-1340 device/oneplus/bacon
+git clone https://github.com/CyanogenMod/android_device_qcom_common.git -b cm-14.0 device/qcom/common
+git clone https://github.com/CyanogenMod/android_device_oppo_common.git -b cm-14.0 device/oppo/common
+git clone https://gitlab.com/amardeep434/vendor_oneplus_aosp -b n7 vendor/oneplus
