3a6fbb1b8aa5e3e55b665644330e82291d90ce3e
Tushar Jain <catusharjain95@gmail.com>
2016-12-24 20:17:05 +0530

    Update cloner.sh

--- cloner.sh
+++ cloner.sh
@@ -5,8 +5,7 @@ rm -rf device/qcom/common
 rm -rf kernel/oneplus/msm8974
 rm -rf vendor/oneplus
 git clone https://github.com/CyanogenMod/android_kernel_oneplus_msm8974 -b cm-14.1 kernel/oneplus/msm8974
-git clone https://github.com/CyanogenMod/android_device_oneplus_bacon -b cm-14.1 device/oneplus/bacon
-git clone https://github.com/CyanogenMod/android_device_qcom_common.git -b cm-14.1 device/qcom/common
+git clone https://github.com/tusharjain95/android_device_oneplus_bacon-1/ -b aosp device/oneplus/bacon
 git clone https://github.com/CyanogenMod/android_device_oppo_common.git -b cm-14.1 device/oppo/common
 git clone https://github.com/AOSPlusone/proprietary_vendor_oneplus.git -b nougat vendor/oneplus
 git clone https://github.com/CyanogenMod/android_external_sony_boringssl-compat.git -b cm-14.1 external/sony/boringssl-compat
