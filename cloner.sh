773a4fdcf73180e0e90dac94ba09557a6873af4c
Dimitris Katikaridis <dkatikaridis@gmail.com>
2017-10-24 13:48:07 +0300

    cloner:fix a typo

--- cloner.sh
+++ cloner.sh
@@ -5,7 +5,7 @@ rm -rf device/qcom/common
 rm -rf kernel/oneplus/msm8974
 rm -rf vendor/oneplus
 git clone https://github.com/nikhil18/lightning-kernel-bacon/ -b lightning-3.4-los kernel/oneplus/msm8974
-git clone https://github.com/LineageOS/android_device_oneplus_bacon -b cm-14.1evice/oneplus/bacon
+git clone https://github.com/LineageOS/android_device_oneplus_bacon -b cm-14.1 device/oneplus/bacon
 git clone https://github.com/LineageOs/android_device_oppo_common.git -b cm-14.1 device/oppo/common
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus -b cm-14.1 vendor/oneplus
 git clone https://github.com/LineageOs/android_external_sony_boringssl-compat.git -b cm-14.1 external/sony/boringssl-compat
